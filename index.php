3679fc60fd38f4b32d95337368e5ba8c9b0463f0
frisian-ganut <76993863+frisian-ganut@users.noreply.github.com>
2021-02-26 13:53:38 +0800

    Update and rename inde to index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+	<title> Today and Tomorrow </title>
+<link rel = "icon" href = "logo.jpg">
+<style>
+	textarea {
+	resize: none;
+	}
+</style>
+</head>
+
+<body>
+<body bgcolor = "#0b1116">
+<body background = "background.png">
+<font face = "verdana" color = "white">
+<table border = "0" width = "1300" height = "500" align = "center" cellspacing = "4" cellpadding = "7" align = "center">
+<tr height = "50">
+<td width = "580"><img src = "logo.jpg" width = "50" alt = "logo"></td>
+	<td align = "center" width = "150" height = "50"><a href = "index.php" title = "Go to Home"><button class = "button" style = "width:100;height:30;background-color:#0b1116;border:0;color:white;"><font size = "4"><b>HOME</b></font></button></a></td>
+
+	<td align = "center" width = "150" height = "50"><a href = "today.php" title = "Go to Today"><button class = "button" style = "width:100;height:30;background-color:#0b1116;border:0;color:white"><font size = "4"><b>TODAY</b></font></button></a></td>
+
+	<td align = "center" width = "150" height = "50"><a href = "tomorrow.php" title = "Go to Tomorrow"><button class = "button" style = "width:130;height:30;background-color:#0b1116;border:0;color:white"><font size = "4"><b>TOMORROW</b></font></button></a></td>
+
+	<td align = "center" width = "150" height = "50"><a href = "contactus.php" title = "Go to Contact"><button class = "button" style = "width:150;height:30;background-color:#0b1116;border:0;color:white"><font size = "4"><b>CONTACT US</b></font></button></a></td>
+
+	<td align = "center" width = "150" height = "50"><a href = "content.php" title = "Go to content"><button class = "button" style = "width:100;height:30;background-color:#0b1116;border:0;color:white"><font size = "4"><b>CONTENT</b></font></button></a></td>
+
+	<td align = "center" width = "30"><a href = "https://twitter.com" title = "Twitter"> <img src = "twitter.jpg" width = "30" height = "30"></a></td>
+
+	<td align = "center" width = "30"><a href = "https://facebook.com" title = "Facebook"> <img src = "facebook.jpg" width = "30" height = "30"></a></td>
+
+	<td align = "center" width = "30"><a href = "https://instagram.com" title = "Instagram"> <img src = "instagram.jpg" width = "30" height = "30"></a></td>
+
+	<td align = "center" width = "30"><a href = "https://youtube.com" title = "YouTube"> <img src = "youtube.jpg" width = "30" height = "30"></a></td>
+</tr>
+
+<tr height = "200" width "1300">
+<td width = "1300" align = "center" colspan = "9"><p align = "center"></p></td>
+</tr>
+
+<tr>
+<td width = "1300" height = "400"></td>
+</tr>
+</table>
+
+<embed src = "yt1s.com - Red Velvet  Psycho Piano Cover Melancholy Ver.mp3" type = "audio/mp3" autoplay = "true"> </embed>
+
+</body>
+</html>
