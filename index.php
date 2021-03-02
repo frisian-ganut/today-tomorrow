86ede89d1dad63c16c7528f45847ffa349156822
frisian-ganut <76993863+frisian-ganut@users.noreply.github.com>
2021-03-02 15:05:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
 <table border = "0" width = "1300" height = "500" cellspacing = "4" cellpadding = "7" align = "center">
 <tr height = "50">
 <td width = "580"><img src = "logo.jpg" width = "50" alt = "logo"></td>
-	<td align = "center" width = "150" height = "50"><a href = "homepage.html" title = "Go to Home"><font size = "4" color = "white"><b>HOME</b></font></a></td>
-	<td align = "center" width = "150" height = "50"><a href = "today.html" title = "Go to Today"><font size = "4" color = "white"><b>TODAY</b></font></a></td>
-	<td align = "center" width = "150" height = "50"><a href = "tomorrow.html" title = "Go to Tomorrow"><font size = "4" color = "white"><b>TOMORROW</b></font></a></td>
-	<td align = "center" width = "200" height = "50"><a href = "createdby.html" title = "Go to Created By"><font size = "4" color = "white"><b>CREATED BY</b></font></a></td>
-	<td align = "center" width = "150" height = "50"><a href = "content.html" title = "Go to Content"><font size = "4" color = "white"><b>CONTENT</b></font></a></td>
+	<td align = "center" width = "150" height = "50"><a href = "index.php" title = "Go to Home"><font size = "4" color = "white"><b>HOME</b></font></a></td>
+	<td align = "center" width = "150" height = "50"><a href = "today.php" title = "Go to Today"><font size = "4" color = "white"><b>TODAY</b></font></a></td>
+	<td align = "center" width = "150" height = "50"><a href = "tomorrow.php" title = "Go to Tomorrow"><font size = "4" color = "white"><b>TOMORROW</b></font></a></td>
+	<td align = "center" width = "200" height = "50"><a href = "createdby.php" title = "Go to Created By"><font size = "4" color = "white"><b>CREATED BY</b></font></a></td>
+	<td align = "center" width = "150" height = "50"><a href = "content.php" title = "Go to Content"><font size = "4" color = "white"><b>CONTENT</b></font></a></td>
 
 	<td align = "center" width = "30"><a href = "https://twitter.com" title = "Twitter"> <img src = "twitter.jpg" width = "30" height = "30"></a></td>
 	<td align = "center" width = "30"><a href = "https://facebook.com" title = "Facebook"> <img src = "facebook.jpg" width = "30" height = "30"></a></td>
